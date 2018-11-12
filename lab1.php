1a9940b2aff7da578366f8f163f00cfd6695392b
Svetikgri <32311098+Svetikgri@users.noreply.github.com>
2018-11-12 21:45:07 +0300

    Add files via upload

--- lab1.php
+++ lab1.php
@@ -1,21 +1,30 @@
-
 <?php
 
-$s1 = "GATATATGCATATACTT";
-$s2 = "ATAT";
+$s1 = trim(fgets(STDIN));
+$s2 = trim(fgets(STDIN));
+
+//$s1 = "ATAT";
+//$s2 = "GATATATGCATATACTT";
+
 
 function main($s1, $s2)
 {
     $count = 0;
-    for ($i=0; $i<strlen($s1); $i++)
-    {
-        $pos = substr($s1, $i,4);
-        if($pos == $s2){
+    //echo $s1;
+    // echo $s2;
+    ///var_dump(strlen($s1)); echo '---';
+    //var_dump(strlen($s2));echo '---';
+    $ras = strlen($s2)-strlen($s1)+1;
+    // echo $ras;echo '---';
+    for ($i=0; $i<$ras; $i++)
+    { //echo $i;
+        $pos = substr($s2, $i,strlen($s1));
+        if($pos == trim($s1)){
             $count ++;
         }
     }
-    echo $count;
-    return $count;
+    fwrite(STDOUT,(int)$count);
+    // $count = fgets(STDOUT);
 
 }
 
